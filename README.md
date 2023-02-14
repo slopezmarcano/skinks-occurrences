af1a15b681e5279d5b2e4ff727beaf95eead4eb9
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-02-14 14:28:45 +1000

    data analyst task with skink occurrence/
    scripts, readme and outputs updates

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # skinks-occurrences
-Skinks are a commonly observed reptile in Australia. 
+Studying sampling bias for the garden skink, using data from ALA for the Data Analyst Task. 
+
+### Task description and objectives
+The minimum convex hull is a way to represent the location and distribution of a species and it is dependent on occurrence points. 
+
+The minimum convex hull is not perfect, has many considerations, and it is suitable for when the occurrence data is abundant and with no bias and error. Outliers and wrong occurrences can really influence the validity of a minimum convex hull. 
+
+Despite these considerations, I wanted to study how the distribution, but primarily the occurrence area of the garden skink has shifted through the years. I purposedly selected human observations, because I wanted to determine if during covid years the ocurrence area of the garden skink decreased. Less people observing maybe resulted in smaller occurrences areas (note considerations of convex polygon)
+
+The overarching prupose of my task was to visualise sampling bias. 
+
+![figure1](outputs/convex_hull_garden_skink_through_years.png)
+
+From this preliminary plot, and considering that the area values are partially incorrect (see code for more detail), it does seem that 2020 and 2021 (covid years) had the highest values of occurrence area for the garden skink. This may have been a result of many spatially separated observations (i.e. making the convex polygon really big) or many people where outside/ in their gardens and providing occurrence data.
+
+From a technical perpestive, my code demonstrates data wrangling using tidyverse and spatial libraries, data visualisation with ggplot. Data gathering was done via APIs and HTTP requests. I created a small function that was then reapplied for many parameter combinations using the lapply function. 
+
+Thank for your time and consideration for this position
+
+
+### Resident skink
+
+This is my resident garden skink 😅
+
+![figure2](outputs/residen_skink.png)
+
+
+
---
+++ scripts/001_skinks_hull.R
@@ -0,0 +1,95 @@
+#Author: Sebastian L
+#Created: 14/03/2023
+#Updated: 14/03/2023
+
+#-- DESCRIPTION --#
+#A script that calculates the area of the minimum convex hull from human observations from 2015 to 2022.
+#The overall objective was to observe if the distribution (convex hull) decreased during covid years. 
+
+
+#-- START TIME --#
+    #11:30 AM AEST
+
+#-- LIBRARIES --# 
+library(rjson) #json wrangling and requests 
+library(jsonlite) #json wrangling
+library(httr) #http request
+library(tidyverse) #data wrangling
+library(sp) #spatial mapping 
+library(geosphere) #spatial wrangling - area calculation
+library(ggthemes) #ggplot themese
+
+#install.packages('rjson', 'jsonlite', 'httr', 'tidyverse', 'sp', 'geosphere', 'ggthemes') #Recommend using CRAN Mirror 2(CSIRO AU)
+
+#-- API REQUEST AND JSON TO DATAFRAME --#
+#api link provided by ALA
+link <- "https://biocache-ws.ala.org.au/ws/occurrences/search?q=lsid%3Ahttps%3A%2F%2Fbiodiversity.org.au%2Fafd%2Ftaxa%2F682e1228-5b3c-45ff-833b-550efd40c399&qualityProfile=ALA&fq=state%3A%22Australian%20Capital%20Territory%22&pageSize=10000"
+
+#get data
+response <- GET(link)
+
+#turn list of lists into simple vectors and obtain content from response into json
+df <- purrr::flatten(fromJSON(content(response, as = 'text'))) 
+
+#if continuing to tibble, you will get an error due to duplicated columns. state vs state.1
+#therefore selecting important sublists from list.
+keydf <- df[c('uuid', 'vernacularName', 'family', 'decimalLatitude', 
+                            'decimalLongitude', 'year', 'basisOfRecord')]
+
+#Convert list into a dataframe
+data <- as_tibble(keydf)
+
+#-- CLEANING AND DATASET WRANGLING --#
+#wrangling data to select skinks, years of interest, human observations only and obsv with years. rename lat and long columns
+data_skinks <- data %>%
+    filter(
+        vernacularName =='Dark-flecked Garden Sunskink',
+        year >=2012 & year <=2022,
+        basisOfRecord == 'HUMAN_OBSERVATION')%>%
+    rename(
+        latitude = decimalLatitude,
+        longitude = decimalLongitude)
+
+#-- OBTAIN UNIQUE YEARS OF OBSERVATION --#
+years <- data_skinks %>%
+    group_by(year) %>%
+    count()
+
+#-- CALCULATE CONVEX HULL --#
+# Calculate the convex hull for each year
+chull_function <- function(data, years){
+    #data frame with occurrences
+    #year column in a dataframe called years for each unique year of observation
+    
+    ds1 <- data %>%
+        filter(year == years) %>%
+        select(longitude, latitude) %>%
+        na.omit() #omit any data with no lat and long
+    
+    hull <- chull(ds1) #convex polygon
+
+    polygons <- as.data.frame(ds1[hull,])
+    
+    ds2<- as.data.frame(geosphere::areaPolygon(polygons))%>% #calculate area of polygon #TODO #3 #2
+        rename(area = 1) %>% #rename column to area
+        mutate(year = years) #add year to the dataframe
+
+    return(ds2)   
+}
+
+#-- RUN THE FUNCTION --#
+area_per_year <- bind_rows(lapply(1:nrow(years), function(x){chull_function(data_skinks, years$year[x])}))
+
+#-- PLOT THE AREAS--#
+p1<-ggplot(area_per_year, aes(x = as.factor(year), y = sqrt(area))) + #TODO: #1 transforming the area to a sqrt just for visualisation of this test. But values are incorrect.
+    geom_segment(aes(x=as.factor(year) ,xend=as.factor(year), y=0, yend=sqrt(area)), color="grey") +
+    geom_point(size=5, color="#69b3a2")  +
+    labs(x = "Year", y = "Area of Convex Hull") +
+    coord_flip()+
+    theme_clean(base_size = 15)
+
+#--SAVE GGPPLOT --#
+ggsave('outputs/convex_hull_garden_skink_through_years.png')
+
+#-- END TIME --#
+    #12:45 PM AEST
